c541596a8ab3fe9fe4d4c2213a9575c0ccb7ed91
Shannon <shannonckendall@gmail.com>
2021-08-28 13:29:16 -0400

    Heroku 4 Life

--- client/src/registerServiceWorker.js
+++ client/src/registerServiceWorker.js
@@ -107,13 +107,3 @@ export function unregister() {
   }
 }
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js').then(function (registration) {
-      console.log('service worker registration succeeded:', registration);
-    },
-    function (error) {
-      console.log('service worker registration failed:', error);
-    });
-} else {
-  console.log('service workers are not supported.');
-}
