8dece6ee3f730fe1cc5f0baa16dec44ffaa2816a
Shannon <shannonckendall@gmail.com>
2023-02-20 12:32:47 -0500

    Right

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -14,11 +12,16 @@ if (process.env.NODE_ENV === "production") {
 }
 
 const mongoose = require("mongoose");
-const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/googlebooks";
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true
-});
+const mongoURL =
+  process.env.PROD_MONGODB || "mongodb://localhost:27017/googlebooks";
+mongoose
+  .connect(mongoURL, { useNewUrlParser: true })
+  .then(() => {
+    console.log("🗄 ==> Successfully connected to mongoDB.");
+  })
+  .catch(err => {
+    console.log(`Error connecting to mongoDB: ${err}`);
+  });
 
 require("./routes/api-routes")(app);
 
