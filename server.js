23590417572db1e3f69936b8bf95c48401a559ad
Shannon <shannonckendall@gmail.com>
2023-02-20 12:00:08 -0500

    I have this file

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-const express = require("express");
 
-const mongoose = require("mongoose");
-const routes = require("./routes");
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -12,15 +10,16 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// Add routes, both API and view
-app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+const mongoose = require("mongoose");
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/googlebooks";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
-// Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-});
+require("./routes/api-routes")(app);
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});   
+
 // Final Project
-  
