33bf81f9d72700194c4f2f4bdbe505c729610362
Shannon <shannonckendall@gmail.com>
2023-02-20 12:21:00 -0500

    file

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -14,12 +16,12 @@ if (process.env.NODE_ENV === "production") {
 const mongoose = require("mongoose");
 const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost:27017/googlebooks";
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true
+});
 
 require("./routes/api-routes")(app);
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
-});   
-
-// Final Project
+});
