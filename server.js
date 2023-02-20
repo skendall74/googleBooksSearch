c25c40eb9f7ef77afb83b931e7522fe62ca3c659
Shannon <shannonckendall@gmail.com>
2023-02-20 17:37:49 -0500

    Server

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -17,3 +19,7 @@ mongoose.connect(MONGODB_URI, {
 });
 
 require("./routes/api-routes")(app);
+
+app.listen(PORT, () => {
+console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
