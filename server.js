b635d09d14bb7fd9d84bc4607cf645f67200602c
Shannon <shannonckendall@gmail.com>
2023-02-20 17:12:41 -0500

    Motha

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
+const path = require("path");
 const PORT = process.env.PORT || 3001;
+const app = express();
+const mongoose = require("mongoose");
+const Book = require("./models/booksModel.js");
+const routes = require("./routes");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -10,14 +12,10 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
-const mongoose = require("mongoose");
-const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/googlebooks";
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-
-require("./routes/api-routes")(app);
-
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
-});   
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+} 
