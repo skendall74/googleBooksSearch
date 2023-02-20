e5541d804ac7aa6e08beb2074a132aa708d39d30
Shannon <shannonckendall@gmail.com>
2023-02-20 17:31:52 -0500

    Wow

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
-const path = require("path");
-const PORT = process.env.PORT || 3001;
+const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
-const Book = require("./models/booksModel.js");
-const routes = require("./routes");
+const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -12,10 +9,11 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-} 
+const mongoose = require("mongoose");
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/googlebooks";
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true
+});
+
+require("./routes/api-routes")(app);
