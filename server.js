0fe8051e127c8a1eef83e57be29fb956211950fd
Shannon <shannonckendall@gmail.com>
2023-02-21 17:07:56 -0500

    Going to push to Heroku and Git

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3001 || 3002
+const PORT = process.env.PORT || 3001 || 
 
 // Define middleware here
 app.use(express.urlencoded({
