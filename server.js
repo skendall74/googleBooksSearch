9180a077fab778b12acdefd27ee4e61785eb3044
Shannon <shannonckendall@gmail.com>
2023-02-21 07:57:54 -0500

    Add Server Change

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
-const path = require("path");
-const PORT = process.env.PORT || 3001;
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
-const Book = require("./models/booksModel.js");
-const routes = require("./routes");
+const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({
   extended: true
 }));
+
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// connect to the mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+const mongoose = require("mongoose");
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/googlebooks";
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true
 });
 
-// Use all routes
-app.use(routes);
+require("./routes/api-routes")(app);
 
 app.listen(PORT, () => {
-      console.log(`🌎 ==> API server now on port ${PORT}!`);
-      
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
+
+// Final Project
