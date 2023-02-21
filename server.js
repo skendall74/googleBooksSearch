87ea47b1cb3a031440b34cb0be382f2da2b8251f
Shannon <shannonckendall@gmail.com>
2023-02-21 09:23:38 -0500

    Impossible

--- server.js
+++ server.js
@@ -1,27 +1,25 @@
-require("dotenv").config();
 const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
 const app = express();
-const mongoose = require("mongoose");
-const Book = require("./models/Book.js");
-const routes = require("./routes/api-routes");
+const PORT = process.env.PORT || 3001;
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/googlebooks", {
+const mongoose = require("mongoose");
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/googlebooks";
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true
 });
 
-// Use all routes
-app.use(routes);
+require("./routes/api-routes")(app);
 
 app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
