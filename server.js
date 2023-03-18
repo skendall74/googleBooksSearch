e2a1b7a35306e85ebdfd8e8ad172c8041ff0d5f9
Shannon Kendall <shannonckendall@gmail.com>
2023-03-18 10:02:50 -0400

    Big Commit

--- server.js
+++ server.js
@@ -11,10 +11,33 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-const mongoose = require("mongoose");
-const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/googlebooks";
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+// const mongoose = require("mongoose");
+// const MONGODB_URI =
+//   process.env.MONGODB_URI || "mongodb://localhost:27017/googlebooks";
+// mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+// Ensure you have run 'npm install mongodb'
+
+var MongoClient = require('mongodb').MongoClient;
+
+var username = 'skendalluf';
+var password = 'rockaura74';
+var hosts = 'iad2-c16-1.mongo.objectrocket.com:54903,iad2-c16-2.mongo.objectrocket.com:54903,iad2-c16-0.mongo.objectrocket.com:54903';
+var database = 'googlebooks';
+var options = '?replicaSet=83c81eceed4341ee9d52dc85853389d7';
+var connectionString = 'mongodb://' + username + ':' + password + '@' + hosts + '/' + database + options;
+
+MongoClient.connect(connectionString, function(err, db) {
+    if (db) {
+        db.close();
+    }
+    if (err) {
+        console.log('Error: ', err);
+    } else {
+        console.log('Connected!');
+        process.exit();
+    }
+});
 
 require("./routes/api-routes")(app);
 
